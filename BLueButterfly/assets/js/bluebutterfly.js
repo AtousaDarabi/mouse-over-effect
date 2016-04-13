d786d383ecb64f326714007b7a4881968576964f
AtousaDarabi <atousa_darabi@yahoo.com>
2016-04-13 10:24:50 -0700

    initial commit

--- BLueButterfly/assets/js/bluebutterfly.js
+++ BLueButterfly/assets/js/bluebutterfly.js
@@ -3,18 +3,18 @@
 
 $(function(){
 
-	$(".butterfly-container").bind({
+	$(".bluebutterfly-container").bind({
 		mouseenter:function(){
-			$(this).children(".butterfly-img").addClass("butterfly-img-down");
-			$(this).children(".butterfly-img").next(".butterfly-content").addClass("butterfly-content-top");
-			$(this).children(".butterfly-img").next(".butterfly-content").children("p.short-description").hide();
-			$(this).children(".butterfly-img").next(".butterfly-content").children("p.long-description").show();
+			$(this).children(".bluebutterfly-img").addClass("bluebutterfly-img-down");
+			$(this).children(".bluebutterfly-img").next(".bluebutterfly-content").addClass("bluebutterfly-content-top");
+			$(this).children(".bluebutterfly-img").next(".bluebutterfly-content").children("p.short-description").hide();
+			$(this).children(".bluebutterfly-img").next(".bluebutterfly-content").children("p.long-description").show();
 		},
 		mouseleave:function(){
-			$(this).children(".butterfly-img").removeClass("butterfly-img-down");
-			$(this).children(".butterfly-img").next(".butterfly-content").removeClass("butterfly-content-top");
-			$(this).children(".butterfly-img").next(".butterfly-content").children("p.short-description").show();
-			$(this).children(".butterfly-img").next(".butterfly-content").children("p.long-description").hide();
+			$(this).children(".bluebutterfly-img").removeClass("bluebutterfly-img-down");
+			$(this).children(".bluebutterfly-img").next(".bluebutterfly-content").removeClass("bluebutterfly-content-top");
+			$(this).children(".bluebutterfly-img").next(".bluebutterfly-content").children("p.short-description").show();
+			$(this).children(".bluebutterfly-img").next(".bluebutterfly-content").children("p.long-description").hide();
 		}
 	});
 
